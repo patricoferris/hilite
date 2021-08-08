1399f27d9204b506ff432ba2c446f7bfdfd7de5d
Patrick Ferris <pf341@patricoferris.com>
2021-08-08 13:55:16 +0100

    bump to latest omd

--- lib/md.mli
+++ lib/md.mli
@@ -1,3 +1,3 @@
-val transform : Omd.block list -> Omd.block list
+val transform : Omd.doc -> Omd.doc
 (** [transform bs] will extract code blocks from [bs] and replace them with HTML
     code blocks with syntax highlighting spans. *)
--- test/lib/test_md.ml
+++ test/lib/test_md.ml
@@ -12,8 +12,8 @@ let omd =
 (* A little fragile -- formatting the html string will break the test *)
 let test_transform () =
   let omd_t = Omd.of_string md |> Hilite.Md.transform in
-  let correct : Omd.block = ({bl_desc = Omd.Html_block {|<pre><code><span class="ocaml-keyword">let </span><span class="ocaml-entity-name">f</span><span class="ocaml-source"> </span><span class="ocaml-source">a</span><span class="ocaml-source"> </span><span class="ocaml-source">b</span><span class="ocaml-source"> </span><span class="ocaml-keyword-operator">=</span><span class="ocaml-source"> </span><span class="ocaml-source">a</span><span class="ocaml-source"> </span><span class="ocaml-keyword-operator">+</span><span class="ocaml-source"> </span><span class="ocaml-source">b</span><span class="ocaml-source"> 
-</span></code></pre>|}; bl_attributes = []})[@@ocamlformat "disable"] in 
+  let correct : 'a Omd.block = Omd.Html_block ([], {|<pre><code><span class="ocaml-keyword">let </span><span class="ocaml-entity-name">f</span><span class="ocaml-source"> </span><span class="ocaml-source">a</span><span class="ocaml-source"> </span><span class="ocaml-source">b</span><span class="ocaml-source"> </span><span class="ocaml-keyword-operator">=</span><span class="ocaml-source"> </span><span class="ocaml-source">a</span><span class="ocaml-source"> </span><span class="ocaml-keyword-operator">+</span><span class="ocaml-source"> </span><span class="ocaml-source">b</span><span class="ocaml-source">
+</span></code></pre>|})[@@ocamlformat "disable"] in 
   Alcotest.check omd "same omd"
     [correct]
     omd_t
