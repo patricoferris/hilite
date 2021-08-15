90055b7aedfcc53ce23ed51415bbe748c11c7c3d
Patrick Ferris <pf341@patricoferris.com>
2021-08-15 10:45:51 +0100

    invert lang check

--- lib/md.ml
+++ lib/md.ml
@@ -1,10 +1,10 @@
-let ignored_langs = [ "bash"; "sh"; ""; "yaml"; "markdown"; "c"; "diff" ]
+let langs = [ "ocaml"; "dune"; "opam" ]
 
 let transform blocks =
   let f (b : 'a Omd.block) =
     match b with
     | Omd.Code_block (attr, lang, src) -> (
-        if List.mem lang ignored_langs then b
+        if not (List.mem lang langs) then b
         else
           match Syntax.src_code_to_html ~lang ~src with
           | Ok html -> Omd.Html_block (attr, html)
