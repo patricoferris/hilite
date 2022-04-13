0f6d01bbcc76a0d2e9b04dc2807076ee92eb1cf9
Patrick Ferris <patrick@sirref.org>
2022-04-13 17:01:54 +0100

    Expose langs

--- src/md.mli
+++ src/md.mli
@@ -1,3 +1,6 @@
+val langs : string list
+(** The list of supported languages for syntax-highlighting. *)
+
 val transform : Omd.doc -> Omd.doc
 (** [transform md] will extract code blocks from [md] and replace them with HTML
     code blocks with syntax highlighting spans. *)
