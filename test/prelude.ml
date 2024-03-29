41939ee3484e321c39fe9c82ecec0fc3e94266ea
Patrick Ferris <patrick@sirref.org>
2024-03-29 13:12:49 +0000

    Fmt

--- test/prelude.ml
+++ test/prelude.ml
@@ -1,5 +1,6 @@
 let read_file path =
-  let rec loop ic acc = match input_line ic with
+  let rec loop ic acc =
+    match input_line ic with
     | line -> loop ic (line :: acc)
     | exception End_of_file -> List.rev acc
   in
